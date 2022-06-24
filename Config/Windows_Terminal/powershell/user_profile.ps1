b7187f679267e1c84a0a089b0b27e9b0bf5bf679
Patt Khwela <pattkhwela@outlook.com>
2022-06-24 11:42:33 +0200

    Commit dotfiles

---
+++ Config/Windows_Terminal/powershell/user_profile.ps1
@@ -0,0 +1,15 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+Set-PoshPrompt paradox
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias y yarn
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
